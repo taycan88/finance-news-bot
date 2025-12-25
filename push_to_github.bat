1eb02f47e1a8a78ebd44ff477f461d0c9588715e
Kike <epedrero_88@hotmail.com>
2025-12-25 08:53:01 +0100

    Update from local PC

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,7 +1,12 @@
 @echo off
-echo --- Actualizando Repositorio en GitHub ---
+echo --- Guardando cambios locales ---
 git add .
 git commit -m "Update from local PC"
+echo.
+echo --- Bajando cambios desde GitHub (Sincronizando) ---
+git pull --rebase --autostash
+echo.
+echo --- Subiendo cambios finales ---
 git push
 echo.
 echo --- Completado! ---
